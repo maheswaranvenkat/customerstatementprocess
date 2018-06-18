945e057c9490c9fe7832af7fc3da4f10cd28cd22
Mahesh <maheswaran.venkatraman@kbc.be>
2018-06-18 13:04:33 +0200

    Javadoc

--- src/main/java/com/rabo/exception/CSVProcessingException.java
+++ src/main/java/com/rabo/exception/CSVProcessingException.java
@@ -1,10 +1,14 @@
 package com.rabo.exception;
 
+/**
+ * Exception class.
+ *
+ * @author Maheswaran Venkatraman
+ * @date 18 June 2018
+ */
 
 public class CSVProcessingException extends Exception {
-
     private static final long serialVersionUID = 1L;
-
     public CSVProcessingException(String message) {
         super(message);
     }
